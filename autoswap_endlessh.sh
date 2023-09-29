8f8cfa96201e1c575f5c5646690ff55c6699279e
0venoven <51714567+0venoven@users.noreply.github.com>
2023-09-29 23:30:26 +0800

    Added install_endlessh function, TODOs and other dependencies

--- autoswap_endlessh.sh
+++ autoswap_endlessh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Function to install dependencies
+# Install dependencies
 install_dependencies() {
     # Run apt update and capture the output in a variable and log file
     update_output=$(sudo apt update 2>&1 | tee /tmp/install_log.txt)
@@ -23,7 +23,7 @@ install_dependencies() {
     fi
 
     # Redirect both stdout and stderr to a log file and execute apt install
-    sudo apt install -y openssh-server build-essential libc6-dev >> /tmp/install_log.txt 2>&1
+    sudo apt install -y openssh-server build-essential libc6-dev git netstat >> /tmp/install_log.txt 2>&1
 
     # Capture the exit status of apt install immediately
     install_status=$?
@@ -37,8 +37,25 @@ install_dependencies() {
     fi
 }
 
+# TODO: change port of real ssh to something preferably higher than port 1024
+
+# Clone, configure, compile and enable endlessh service
+install_endlessh() {
+    git clone https://github.com/skeeto/endlessh
+    mkdir ~/endlessh
+    cd ~/endlessh
+    make
+    sudo mv endlessh /usr/local/bin/
+    sudo cp util/endlessh.service /etc/systemd/system/
+    sudo systemctl enable endlessh
+    sudo systemctl daemon-reload
+    sudo systemctl start endlessh
+}
+
 # Main script
 echo "Setting up Endlessh on Ubuntu..."
 
-# Call the function
 install_dependencies
+# TODO: change ssh port first
+install_endlessh
+# TODO: maybe netstat and get verification that the endlessh and real ssh services are running
